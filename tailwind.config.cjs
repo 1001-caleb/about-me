02923a5a3968001de6d474a015556c0793adc88f
Caleb Parrra <mccalebyt@gmail.com>
2022-08-29 09:50:20 -0500

    daisy ui theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+
+					"primary": "#fffffe",
+
+					"secondary": "#72757e",
+
+					"accent": "#dd6361",
+
+					"neutral": "#251C26",
+
+					"base-100": "#16161a",
+
+					"info": "#34BDEF",
+
+					"success": "#2cb67d",
+
+					"warning": "#7f5af0",
+
+					"error": "#7f5af0",
+				},
+			},
+		],
 	},
-	plugins: [],
+
+	plugins: [require("daisyui")],
 }
